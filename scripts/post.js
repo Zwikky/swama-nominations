f273992d9f47984d88cfc081d7b4f0fd07dd339e
Zwakele Mkhatshwa <M5060885@fnb.co.za>
2020-10-14 16:21:26 +0200

    Post Data

---
+++ scripts/post.js
@@ -0,0 +1,25 @@
+function ajax_post() {
+ 
+    const xhr = new XMLHttpRequest();
+    const url = "http://34.72.209.20/systems/zet/swama/api/receive.php";
+    // const url = "http://localhost/awards/api/receive.php";
+    var cellphone = document.getElementById("cellphone").value;
+    var category = document.getElementById("category").value;
+    var nominee = document.getElementById("nominee").value;
+    var params = "cellphone="+cellphone+"&category="+category+"&nominee="+nominee;
+    
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var return_data = xhr.responseText;
+            document.getElementById("status").innerHTML = return_data;
+        }
+        if (xhr.readyState == 1 && xhr.status == 0) {
+            var return_data = xhr.responseText;
+            document.getElementById("status").innerHTML = "Error Nominating, Try Again Later";
+        }
+    }
+    xhr.send(params);
+    document.getElementById("status").innerHTML = "processing...";
+}
